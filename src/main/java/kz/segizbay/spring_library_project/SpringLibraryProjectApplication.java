5cf7b210b458ac60413a38861a6771d553c399ff
Madiyar Segizbay <m.segizbay@aues.kz>
2024-09-15 20:02:13 +0500

    First commit

---
+++ src/main/java/kz/segizbay/spring_library_project/SpringLibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.segizbay.spring_library_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/segizbay/spring_library_project/SpringLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.segizbay.spring_library_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
